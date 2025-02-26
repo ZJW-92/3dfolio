aff837a8ff53896c401e98eafb498ebced571da1
ZJW-92 <zjwei0201@gmail.com>
2025-02-26 15:12:46 +0100

    change 3d animation

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,9 +6,8 @@ import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   //const computer = useGLTF("./desktop_pc/scene.gltf");
-  const computer = useGLTF("./gaming_desktop_pc/scene.gltf");
-  
-
+  //const computer = useGLTF("./gaming_desktop_pc/scene.gltf");
+  const computer = useGLTF("./cooker/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
@@ -23,7 +22,10 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 7 : 20}
+        // the following line is for scale for gaming desktop 
+         // scale={isMobile ? 0.7 : 0.75}
+      
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
