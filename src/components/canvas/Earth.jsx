3c9d6e4579721b9715608d1cd8d36ee334f1812f
ZJW-92 <zjwei0201@gmail.com>
2025-02-26 19:25:29 +0100

    change 3d animation

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,12 +5,12 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
- const earth = useGLTF("./planet/scene.gltf");
+ const earth = useGLTF("./rice/scene.gltf");
  //const earth = useGLTF("./planet_earth/scene.gltf");
 
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={12} position-y={0} rotation-y={0} rotation-z={0.7} />
   );
 };
 
